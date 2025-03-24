a430e555691fbad00e2e53c5ce9bf8d434f8f6ca
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-03-24 19:57:10 +0530

    day-6

---
+++ Leetcode/day-6.py
@@ -0,0 +1,20 @@
+
+# Merge Two Sorted Arrays In-Place
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+nums3  = []
+for i in range(m):
+     nums3.append(nums1[i])
+
+for j in range(n):
+     nums3.append(nums2[j])
+
+nums3.sort()
+
+for val in range(len(nums3)):
+     nums1[val]=nums3[val]
+print(nums1)
+
+
