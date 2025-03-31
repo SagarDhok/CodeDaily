5dbca081a8c050c6075a06e82b54bfd3a6d45697
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-03-31 19:38:06 +0530

    day-13

--- GreeksOfGreeks/day-12.py
+++ GreeksOfGreeks/day-12.py
@@ -1,6 +1,6 @@
 # Palindromic Array
-# arr = [111, 222, 333, 444, 555]
-# for i in arr:
-#             if str(i) == str(i)[::-1]:
-#                 print(True)
-# print(False)
+arr = [111, 222, 333, 444, 555]
+for i in arr:
+            if str(i) == str(i)[::-1]:
+                print(True)
+print(False)
