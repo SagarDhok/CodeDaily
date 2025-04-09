e2df9370168b2d438c1a13b6610ca142dd4ad730
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-09 21:18:08 +0530

    Message

--- Leetcode/day-16.py
+++ Leetcode/day-16.py
@@ -1,18 +1,36 @@
+# class Solution:
+#     def removeDuplicates(self, nums):
+#         if not nums:
+#             return 0
+        
+#         k = 1  
+#         for i in range(1, len(nums)):
+#             if nums[i] != nums[i - 1]:
+#                 nums[k] = nums[i]
+#                 k += 1
+        
+#         return k
+
+
+# obj = Solution()
+# nums = [1,1,2]
+# res = obj.removeDuplicates(nums)
+# print(res)
+
+
 class Solution:
+
     def removeDuplicates(self, nums):
-        if not nums:
-            return 0
-        
-        k = 1  
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[k] = nums[i]
-                k += 1
-        
-        return k
-
-
-obj = Solution()
+       unique = []
+       for i in nums : 
+           if i not in unique:
+               unique.append(i)
+      
+       for i in range(len(unique)):
+           nums[i] = unique[i]
+       
+       return len(unique)
+s=Solution()    
 nums = [1,1,2]
-res = obj.removeDuplicates(nums)
-print(res)
+result = s.removeDuplicates(nums)
+print(result)
