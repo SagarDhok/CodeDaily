031d2db66d1e9d7ffc67926ee3af21d58ce3e2d3
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-12 11:22:50 +0530

    Message

--- Leetcode/day-19.py
+++ Leetcode/day-19.py
@@ -1,17 +1,14 @@
 
-# def addBinary(a,b) -> str:
-#     # Convert binary strings to integers
-#     num1 = int(a, 2)
-#     num2 = int(b, 2)
+def addBinary(a,b) -> str:
+    # Convert binary strings to integers
+    num1 = int(a, 2)
+    num2 = int(b, 2)
     
    
-#     total = num1 + num2
+    total = num1 + num2
     
-#     return bin(total)[2:]
+    return bin(total)[2:]
 
-# print(addBinary("11", "1"))    
-# print(addBinary("1010", "1011"))  
-a = "1010"
-b = "1011"
+print(addBinary("11", "1"))    
+print(addBinary("1010", "1011"))  
 
-print(bin(int(a,2)+int(b,2))[2:])
---
+++ Leetcode/day-20.py
@@ -0,0 +1,18 @@
+
+# Contains Duplicate
+# Array Duplicates
+arr = [1,8,4,3,6,9,5,7,2,7,0]
+
+count = {}
+dno = []
+
+for i in arr:
+  if i not in count:
+    count[i]= 1
+  else:
+    count[i]+=1
+    
+for j in count:
+  if count[j]>=2:
+       dno.append(j)
+print(dno)
