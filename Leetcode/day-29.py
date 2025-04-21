7cfbf4f0e5a003075976cf86f0956c67aef854af
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-21 07:58:44 +0530

    day-34

--- Leetcode/day-29.py
+++ Leetcode/day-29.py
@@ -1,18 +1,39 @@
 
 # Move Zeroes
-class Solution:
-    def moveZeroes(self, nums) :
+# class Solution:
+#     def moveZeroes(self, nums) :
         
-        for i in nums[:]:
-            print(i)
-            if i == 0:
-             nums.append(0)
-             nums.remove(0)
-        return nums
-s= Solution()
-nums =  [int(i) for i in input("Enter List Of Values : ").split()]
-res = s.moveZeroes(nums)
-print(res)
+#         for i in nums[:]:
+#             print(i)
+#             if i == 0:
+#              nums.append(0)
+#              nums.remove(0)
+#         return nums
+# s= Solution()
+# nums =  [int(i) for i in input("Enter List Of Values : ").split()]
+# res = s.moveZeroes(nums)
+# print(res)
+
+
+
+
+# class Solution:
+#     def moveZeroes(self, nums) :
+#        nzi = 0
+#        for i in range(len(nums)) :
+#            if nums[i]!= 0:
+#                nums[nzi] = nums[i]
+#                nzi+=1
+#        for i in range(nzi,len(nums)):
+#            nums[i] = 0
+#        return nums
+       
+# s= Solution()
+# nums =  [int(i) for i in input("Enter List Of Values : ").split()]
+# res = s.moveZeroes(nums)
+# print(res)
+
+
 
 
 
@@ -22,18 +43,12 @@ class Solution:
        nzi = 0
        for i in range(len(nums)) :
            if nums[i]!= 0:
-               nums[nzi] = nums[i]
+               nums[nzi],nums[i] = nums[i],nums[nzi]
                nzi+=1
-       for i in range(nzi,len(nums)):
-           nums[i] = 0
        return nums
        
+       
 s= Solution()
-nums =  [int(i) for i in input("Enter List Of Values : ").split()]
+nums = [int(i) for i in input("Enter List Of Values : ").split()]
 res = s.moveZeroes(nums)
 print(res)
-
-
-
-
-
