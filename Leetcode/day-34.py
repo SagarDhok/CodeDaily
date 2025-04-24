e9786e57e565a299eb580ad166227a7714f77b80
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-24 08:54:17 +0530

    day-34

--- Leetcode/day-34.py
+++ Leetcode/day-34.py
@@ -1,17 +1,21 @@
-
-
-# Max Consecutive Ones
-def findMaxConsecutiveOnes(nums):
- mc = 0 
- cc = 0
  
- for i in nums:  
-   if i == 1:
-     cc +=1
-   else:
-    mc =  max(mc,cc)
-    cc = 0
- return max(mc,cc)
-  
-# print(findMaxConsecutiveOnes( [1,1,0,1,1,1]))
-print(findMaxConsecutiveOnes( [1,0,1,1,0,1]))
+#! Max Consecutive Ones
+def findMaxConsecutiveOnes(nums):
+    mc = 0  
+    cc = 0  
+
+    for i in nums:
+        if i == 1:
+            cc += 1
+        else:
+            if cc > mc:
+                mc = cc
+            cc = 0
+
+    if cc > mc:
+        mc = cc
+
+    return mc
+
+print(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))  
+print(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]))  
