33552165fee42713a67b2670957b99ec4668bfec
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-24 08:17:36 +0530

    day-37

--- GreeksOfGreeks/day-37.py
+++ GreeksOfGreeks/day-37.py
@@ -1,9 +1,20 @@
 
-arr= arr = [int(i) for i in input("Enter List Of Values : ").split()]
-d = int(input("Enter A Number : "))
-d = d % len(arr)
-arr[:]  = arr[d:]+arr[:d]
-print(arr[:])
+#!Rotating an Array
+class Solution:
+    def leftRotate(self, arr, d):
+        n = len(arr)
+        d = d % n  
+        arr[:] = arr[d:] + arr[:d]  
+arr = [1, 2, 3, 4, 5]
+Solution().leftRotate(arr, 2)
+print(arr)
 
 
-
+#APPROACH - 2
+arr= [-1, -2, -3, 4, 5, 6, 7]
+d = 2
+for i in range(d):
+   arr.append(arr[0])
+   arr.remove(arr[0])
+   i +=1
+print(arr)
