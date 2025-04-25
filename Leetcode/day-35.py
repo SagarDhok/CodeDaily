17a3997ec32278026f903fe72f46dcbd4b139d0e
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-25 15:51:51 +0530

    day-35

--- Leetcode/day-35.py
+++ Leetcode/day-35.py
@@ -11,3 +11,19 @@ s = Solution()
 print(s.nextGreatestLetter(["c","f","j"], "a")) 
 print(s.nextGreatestLetter(["c","f","j"], "c"))  
 print(s.nextGreatestLetter(["x","x","y","y"], "z"))  
+
+
+
+#APPROACH - 2
+def nextGreatestLetter(letters, target):
+    low,= 0
+    high=len(letters) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if letters[mid] <= target:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return letters[low % len(letters)]
