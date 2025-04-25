023205670b1f7da1cf0c1ca1f8b5b36bdaaf912e
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-25 15:50:32 +0530

    day-35

--- Leetcode/day-35.py
+++ Leetcode/day-35.py
@@ -1,8 +1,13 @@
 
-# Find Smallest Letter Greater Than Target
+#! Find Smallest Letter Greater Than Target
+#APPROACH - 1
 class Solution:
     def nextGreatestLetter(self, letters, target):
         for letter in letters:
             if letter > target:
                 return letter
         return letters[0]  
+s = Solution()
+print(s.nextGreatestLetter(["c","f","j"], "a")) 
+print(s.nextGreatestLetter(["c","f","j"], "c"))  
+print(s.nextGreatestLetter(["x","x","y","y"], "z"))  
