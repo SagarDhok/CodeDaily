52dfa38e88cb89181b964045ec368910f9b23b4e
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-25 15:31:10 +0530

    day-38

--- GeeksOfGeeks/day-38.py
+++ GeeksOfGeeks/day-38.py
@@ -1,2 +1,12 @@
 
 #! Count of smaller elements
+class Solution:
+    def countOfElements(self, x, arr):
+        count = 0
+        for i in arr:
+            if i<=x:
+                count+=1
+        return count
+s = Solution()
+print(s.countOfElements(9,[10, 1, 2, 8, 4, 5] ))
+print(s.countOfElements(2,[1, 2, 2, 5, 7, 2, 9]  ))
