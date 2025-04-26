1d4b58329ae8d9d98d4688670afe1ccbb5d56b81
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-26 06:06:42 +0530

    day-36

--- Leetcode/day-36.py
+++ Leetcode/day-36.py
@@ -1,16 +1,20 @@
 
 #! Next Greater Element I
-nums1 = [4,1,2]
-nums2 = [1,3,4,2]
-res = []
 
-for num in nums1:
-    idx = nums2.index(num)
-    found = -1
-    for j in range(idx + 1, len(nums2)):
-        if nums2[j] > num:
-            found = nums2[j]
-            break
-    res.append(found)
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        res = []
+        
+        for num in nums1:
+            idx = nums2.index(num)
+            found = -1
+            for j in range(idx + 1, len(nums2)):
+                if nums2[j] > num:
+                    found = nums2[j]
+                    break
+            res.append(found) 
+        
+        return res
 
-print(res)  
+sol = Solution()
+print(sol.nextGreaterElement([4,1,2], [1,3,4,2])) 
