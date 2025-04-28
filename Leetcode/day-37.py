e3724765613a37ebf28c3eab1ff5d8317b24b01f
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-04-28 19:41:20 +0530

    day-37

--- Leetcode/day-37.py
+++ Leetcode/day-37.py
@@ -1,2 +1,22 @@
 
 # Add Digits
+#APPRAOCH - 1
+class Solution:
+    def addDigits(self, num: int) -> int:
+        s = 0
+
+        while num >= 10:
+            while num > 0:
+                digit = num % 10  
+                s = s + digit  
+                num = num // 10  
+            
+            num = s  
+            s = 0  
+        
+        return num  
+s = Solution()
+print(s.addDigits(38))  
+print(s.addDigits(0)) 
+
+
