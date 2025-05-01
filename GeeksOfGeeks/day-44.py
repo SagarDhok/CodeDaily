2114aec00932dde5251e75e9f9913dd74a9b9d9d
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-01 07:59:27 +0530

    day-44

---
+++ GeeksOfGeeks/day-44.py
@@ -0,0 +1,16 @@
+
+
+class Solution:
+    def longest(self, arr):
+        max_len = 0
+        longest_word = ""
+        for word in arr:
+            if len(word) > max_len:
+                max_len = len(word)  
+                longest_word = word
+        return longest_word
+
+
+s = Solution()
+print(s.longest(["Geek", "Geeks", "Geeksfor", "GeeksforGeek", "GeeksforGeeks", "GeeksforGeeks"]))
+print(s.longest(["Apple", "Mango", "Orange", "Banana"]))  
