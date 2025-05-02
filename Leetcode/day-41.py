413afd27f5f22d509bb5722763fb722e4b96d720
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-02 09:09:06 +0530

    day-41

---
+++ Leetcode/day-41.py
@@ -0,0 +1,19 @@
+
+
+class Solution:
+    def isUgly(self, n):
+        if n <= 0:
+            return False
+        
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n = n // p
+        
+        if n == 1:
+            return True
+        else:
+            return False
+        
+s = Solution()
+n = int(input("Enter a Number : "))
+print(s.isUgly(n))
