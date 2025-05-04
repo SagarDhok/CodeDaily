0c4479626c78a76246d5a86024c8c836cd9d79b8
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-04 07:57:32 +0530

    day-43

---
+++ Leetcode/day-43.py
@@ -0,0 +1,10 @@
+
+# Is Subsequence
+def isSubsequence(self, s: str, t: str) -> bool:
+            i = 0
+            for char in t:
+                if i < len(s) and s[i] == char:
+                    i += 1
+            return i == len(s)
+print(isSubsequence("abc", "ahbgdc"))  
+print(isSubsequence("axc", "ahbgdc"))  
