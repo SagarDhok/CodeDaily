cf409ebe03f8a314a0097de02ba7beac0f8346e5
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-04 07:42:43 +0530

    day-47

--- Leetcode/day-42.py
+++ Leetcode/day-42.py
@@ -1,21 +1,30 @@
 
 # Find the Difference
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        freq = [0] * 26  
+# class Solution:
+#     def findTheDifference(self, s: str, t: str) -> str:
+#         freq = [0] * 26  
 
-        for ch in s:
-            freq[ord(ch) - ord('a')] += 1
+#         for ch in s:
+#             freq[ord(ch) - ord('a')] += 1
+#             print(freq[ord(ch) - ord('a')])
 
-        for ch in t:
-            freq[ord(ch) - ord('a')] -= 1
-            if freq[ord(ch) - ord('a')] < 0:
-                return ch
-s = "a"
-t = "aa"
-sol = Solution()
-print(sol.findTheDifference(s, t)) 
+#         for ch in t:
+#             freq[ord(ch) - ord('a')] -= 1
+#             if freq[ord(ch) - ord('a')] < 0:
+#                 return ch
+# s = "a"
+# t = "aa"
+# sol = Solution()
+# print(sol.findTheDifference(s, t)) 
 
-s = "abcd"
-t = "abcde"
-print(sol.findTheDifference(s, t)) 
+# print(sol.findTheDifference(s, t)) 
+
+def find(s,t):
+   for i in t:
+       if i not in s:
+           return i
+       elif s.count(i)<t.count(i):
+           return i
+s = "ab"
+t = "abc"
+print(find(s,t))
