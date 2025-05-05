99a95cbbb42745ff30f3e0ce7aa6f3ec15d3b8f8
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-05 07:38:48 +0530

    day-44

---
+++ Leetcode/day-44.py
@@ -0,0 +1,18 @@
+
+
+# Perfect Number
+class Solution:
+    def checkPerfectNumber(self, num):
+        if num <= 1:
+            return False
+        s = 1
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                s += i
+                if i != num // i:
+                    s += num // i
+        return s == num
+
+s = Solution()
+num = int(input("Enter A number : "))
+print(s.checkPerfectNumber(num))
