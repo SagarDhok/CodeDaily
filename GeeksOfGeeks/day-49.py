13cc4a40a9b04d334ce960c8e1a7417444590c6b
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-06 14:05:46 +0530

    day-49

---
+++ GeeksOfGeeks/day-49.py
@@ -0,0 +1,23 @@
+
+
+
+# Palindrome Array
+#APPROACH - 1
+def is_palindrome(arr):
+  left = 0
+  right = len(arr)-1
+  while left<right:
+    if arr[left]!=arr[right]:
+      return False
+    left +=1
+    right -=1
+  return True
+print(is_palindrome([1, 2, 3, 2, 1])) 
+print(is_palindrome([1, 2, 3, 4, 5])) 
+
+
+# APPROACH - 2
+arr = [1,2,3,4,5]
+print(arr==arr[::-1])
+
+
