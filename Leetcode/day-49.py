9d0a00e118f4a9eff06c0ee63f0e91905478a5d8
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-10 12:25:30 +0530

    day-49

--- Leetcode/day-49.py
+++
@@ -1,9 +0,0 @@
-
-
-# Convert a Number to Hexadecimal
-class Solution:
-    def toHex(self, num: int) -> str:
-            return hex(num & 0xFFFFFFFF)[2:]
-s = Solution()
-num = int(input("Enter A Number : "))
-print(s.toHex(num))
