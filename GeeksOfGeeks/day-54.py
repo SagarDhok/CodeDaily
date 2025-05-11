1816e39b5003d31c71bac6fbf60d96ea361359bc
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-11 14:02:45 +0530

    day-54

--- GeeksOfGeeks/day-54.py
+++ GeeksOfGeeks/day-54.py
@@ -1,11 +1,19 @@
 
-def fun(arr):
- 
- for i in range(len(arr)):
-  if arr[i] == 1:
-   return i
-  
- return -1
-  
+# First 1 in a Sorted Binary Array
+
+class Solution:
+    def firstIndex(self, arr):
+                for i in range(len(arr)):
+                 if arr[i] == 1:
+                  return i
+                
+                return -1
+                
 arr = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1]
-print(fun(arr))
+s =Solution()
+print(s.firstIndex(arr))
+
+                
+arr = [0, 0, 0, 0]
+s =Solution()
+print(s.firstIndex(arr))
