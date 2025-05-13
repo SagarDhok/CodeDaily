5b67e32425dc6a6723cf3ced4c9578e21efc96c5
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-13 08:07:32 +0530

    day-52

--- Leetcode/day-52.py
+++ Leetcode/day-52.py
@@ -1,7 +1,8 @@
 
+
 class Solution:
     def canWinNim(self, n) :
         return n % 4 != 0
 
 s = Solution()
-print(s.canWinNim(int(input("Enter A Number : "))))
+print(s.canWinNim(int(input("Enter A Number : ")))
