523aaf38938df5ca51c7def3b2c3057c8f94691a
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-14 07:05:53 +0530

    day-53

--- Leetcode/day-53.py
+++ Leetcode/day-53.py
@@ -1,3 +1,13 @@
 
 
-vv
+# Arranging Coins
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        i = 1
+        while n >= i:
+            n -= i
+            i += 1
+        return i - 1
+s = Solution()
+print(s.arrangeCoins(n = int(input("Enter A number : " ))))
