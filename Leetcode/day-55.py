0e51b809d4335ae401a86342b3c4b4ccf1bcd2ed
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-16 11:48:53 +0530

    day-55

---
+++ Leetcode/day-55.py
@@ -0,0 +1,13 @@
+
+
+
+# Pow(x, n)
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        
+s = Solution()
+x = float(input("Enter x : "))
+n = float(input("Enter y : "))
+print(s.myPow(x,n))
