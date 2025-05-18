ee6d6de3ac6f3800cf4167cb637c4b814fef52ef
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-18 07:12:01 +0530

    day-62

---
+++ GeeksOfGeeks/day-62.py
@@ -0,0 +1,19 @@
+
+
+
+class Solution:
+ def reverseSubArray(self,arr,l,r):
+       arr[l-1:r]=arr[l-1:r][::-1]
+       return arr
+ 
+s = Solution()
+arr = [1, 2, 3, 4, 5, 6, 7]
+l = 2
+r = 4
+print(s.reverseSubArray(arr,l,r))
+
+
+arr = [1, 6, 7, 4]
+l = 1
+r = 4
+print(s.reverseSubArray(arr,l,r))
