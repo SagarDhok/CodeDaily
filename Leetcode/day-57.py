ef9889b4c562606dfc89141da81bf8e51a94179d
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-18 12:27:53 +0530

    day-57

---
+++ Leetcode/day-57.py
@@ -0,0 +1,19 @@
+
+
+class Solution:
+    def heightChecker(self, heights):
+        expected = heights[::]
+        expected.sort()
+
+
+        i=0
+        count = 0
+        while i<len(heights) and i<len(expected):
+                        if heights[i]!=expected[i]:
+                                count+=1
+                        i +=1 
+        return count
+    
+s = Solution()
+print(s.heightChecker([1,1,4,2,1,3]))
+print(s.heightChecker([5,1,2,3,4]))
