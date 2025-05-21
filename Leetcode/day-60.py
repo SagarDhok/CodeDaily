e01c2d2cce311ed127093394b6893213ec31d336
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-21 20:29:21 +0530

    day-60

--- Leetcode/day-60.py
+++ Leetcode/day-60.py
@@ -12,3 +12,8 @@ class Solution:
             if nums[i] != i + 1:
                 return i + 1
         return n + 1
+
+sol = Solution()
+print(sol.firstMissingPositive([1, 2, 0]))         
+print(sol.firstMissingPositive([3, 4, -1, 1]))     
+print(sol.firstMissingPositive([7, 8, 9, 11, 12]))
