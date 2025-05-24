5652648f062512d021beaf31601c7197885b2198
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-24 10:35:30 +0530

    day-63

---
+++ Leetcode/day-63.py
@@ -0,0 +1,22 @@
+
+
+
+def canPlaceFlowers(flowerbed, n):
+    count = 0
+    length = len(flowerbed)
+
+    for i in range(length):
+        if flowerbed[i] == 0:
+            empty_left = (i == 0) or (flowerbed[i - 1] == 0)
+            empty_right = (i == length - 1) or (flowerbed[i + 1] == 0)
+
+            if empty_left and empty_right:
+                flowerbed[i] = 1  
+                count += 1
+                if count >= n:
+                    return True
+
+    return count >= n
+
+print(canPlaceFlowers([1, 0, 0, 0, 1], 1))  
+print(canPlaceFlowers([1, 0, 0, 0, 1], 2))  
