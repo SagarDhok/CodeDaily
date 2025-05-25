bcc0a11290f54ba69a9af4204f28e8e1ed0bbf4e
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-25 07:56:03 +0530

    day-69

---
+++ GeeksOfGeeks/day-69.py
@@ -0,0 +1,24 @@
+
+
+# Make a Distinct Digit Array
+
+
+
+class Solution:
+   def common_digits(self, nums):
+        st =set()
+        for i in nums:
+         n = i
+         while n>0:
+           d = n%10
+           if d not in st:
+             st.add(d)
+           n=n//10
+        return sorted(st)
+		   
+s = Solution()
+nums = [131, 11, 48]
+print(s.common_digits(nums))
+
+nums =[111, 222, 333, 4444, 666]
+print(s.common_digits(nums))
