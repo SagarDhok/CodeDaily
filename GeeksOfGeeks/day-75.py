7810ed5d66847f93408dcd86414bc76c3964c891
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-05-31 09:51:32 +0530

    day-75

---
+++ GeeksOfGeeks/day-75.py
@@ -0,0 +1,22 @@
+
+
+class Solution:
+    def chocolates(self, arr) :
+        start = 0
+        end = len(arr) - 1
+        
+        while start < end:
+            if arr[start] > arr[end]:
+                start += 1
+            else:
+                end -= 1
+        
+        return arr[start]
+
+s = Solution()
+
+arr = [5, 3, 1, 6, 9]
+print(s.chocolates(arr))
+
+arr = [5, 9, 2, 6]
+print(s.chocolates(arr))
