7f7350776a6a757a89d481a3805e82b64c97854f
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-06-01 07:48:00 +0530

    day-76

--- GeeksOfGeeks/day-76.py
+++ GeeksOfGeeks/day-76.py
@@ -1,2 +1,14 @@
 
 #!  At least two greater elements
+
+class Solution:
+    def findElements(self,arr):
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                if arr[i]>arr[j]:
+                    arr[i],arr[j]=arr[j],arr[i]
+        return arr[:-2]
+
+s = Solution()
+arr = [int(i) for i in input("Enter array Elements : ").split()]
+print(s.findElements(arr))
