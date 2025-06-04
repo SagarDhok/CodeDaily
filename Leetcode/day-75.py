171bc4eaa507704b4f365dfa2c3552c572a65c2b
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-06-04 21:08:13 +0530

    day-75

--- Leetcode/day-75.py
+++ Leetcode/day-75.py
@@ -25,11 +25,3 @@ class Solution:
 
         return result
 
-
-sol = Solution()
-print(sol.insert([[1, 3], [6, 9]], [2, 5]))
-print(sol.insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
-print(sol.insert([], [5, 7]))
-print(sol.insert([[1, 5]], [2, 3]))
-print(sol.insert([[1, 5]], [2, 7]))
-print(sol.insert([[1, 5]], [6, 8]))
