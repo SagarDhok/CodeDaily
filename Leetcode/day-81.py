f26a78eb71e03043c50063c9df006e682a911b85
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-06-09 20:25:22 +0530

    day-81

---
+++ Leetcode/day-81.py
@@ -0,0 +1,55 @@
+
+# Add Two Numbers
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            digit = total % 10
+
+            current.next = ListNode(digit)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+
+def list_to_linked_list(lst):
+    dummy = ListNode()
+    current = dummy
+    for num in lst:
+        current.next = ListNode(num)
+        current = current.next
+    return dummy.next
+
+def linked_list_to_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+l1 = list_to_linked_list([2, 4, 3])
+l2 = list_to_linked_list([5, 6, 4])
+
+sol = Solution()
+result_node = sol.addTwoNumbers(l1, l2)
+
+print(linked_list_to_list(result_node))
