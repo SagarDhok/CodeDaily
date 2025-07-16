405c96f88910b3caeed4dba9e790c89f5344a8b2
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-07-16 09:44:12 +0530

    Day-106

---
+++ Leetcode/day-106.py
@@ -0,0 +1,20 @@
+
+
+
+#! 766. Toeplitz Matrix
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for i in range(rows - 1):
+            for j in range(cols - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
+                    return False
+        return True
+
+matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+print(Solution().isToeplitzMatrix(matrix))  
+
+matrix = [[1,2],[2,2]]
+print(Solution().isToeplitzMatrix(matrix))
