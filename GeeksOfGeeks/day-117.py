98bbed7d9419ef7d62765a58e3fd6c26ee40391a
sagardhok9 <160725777+sagardhok9@users.noreply.github.com>
2025-07-31 07:06:20 +0530

    Day-117

--- GeeksOfGeeks/day-117.py
+++ GeeksOfGeeks/day-117.py
@@ -1,13 +1,16 @@
 
 
-#! Farthest Index
+# #! Farthest Index
+
+#User function Template for python3
 
 class Solution:
     def findIndex(self, arr, x):
-        for i in range(-1,-len(arr),-1):
-            if arr[i]==x:
-                return x
+        for i in range(len(arr)-1, -1, -1):
+            if arr[i] == x:
+                return i+1
         return -1
+
 s = Solution()
 arr= [7, 42, 5, 6, 42, 8, 7, 5, 3, 6, 7] 
 x = 7
