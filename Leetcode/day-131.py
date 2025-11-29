d6919adffe60988df64a99d5f1a2344327a1024b
sagardhok <sdhok041@gmail.com>
2025-11-29 21:37:26 +0530

    Day-131

---
+++ Leetcode/day-131.py
@@ -0,0 +1,44 @@
+
+
+#! 961. N-Repeated Element in Size 2N Array
+class Solution:
+    def repeatedNTimes(self, nums) :
+        for i in range(len(nums)):
+            if nums[i] == nums[i-1] or nums[i] == nums[i-2]:
+                return nums[i]
+
+s  = Solution()
+nums = [1,2,3,3]
+print(s.repeatedNTimes(nums))
+
+nums = [2,1,2,5,3,2]
+print(s.repeatedNTimes(nums))
+
+nums = [5,1,5,2,5,3,5,4]
+print(s.repeatedNTimes(nums))
+
+
+class Solution:
+    def repeatedNTimes(self, nums) :
+        n = len(nums)/2
+        freq = {}
+        for i in nums:
+            if i not in freq:
+                freq[i]=1
+            else:
+                freq[i]+=1
+
+        for k,v in freq.items():
+            if n==v:
+                return k
+            
+
+s  = Solution()
+nums = [1,2,3,3]
+print(s.repeatedNTimes(nums))
+
+nums = [2,1,2,5,3,2]
+print(s.repeatedNTimes(nums))
+
+nums = [5,1,5,2,5,3,5,4]
+print(s.repeatedNTimes(nums))
