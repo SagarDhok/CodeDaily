50fdd0d25476be747a9b47f1f219ad6ccaff9a93
sagardhok <sdhok041@gmail.com>
2025-12-30 08:58:00 +0530

    Q2

--- Questions.py
+++ Questions.py
@@ -17,3 +17,68 @@ while left<right:
     right-=1
 
 print(s)
+
+
+#! 2. Valid Palindrome
+# Return True if a string is a palindrome, considering only alphanumeric characters and ignoring case.
+# Example:
+# "A man, a plan, a canal: Panama" → True
+
+s = "A man, a plan, a canal: Panama"
+left = 0
+right = len(s)-1
+while left<right:
+    while left<right and not s[left].isalnum():
+        left+=1
+
+    while left<right and not s[right].isalnum():
+        right-=1
+    
+    if s[left].lower()!=s[right].lower():
+        print(False)
+        break
+    left+=1
+    right-=1
+
+else:
+    print(True)
+
+#* Without Built in Funtion
+s = "A man, a plan, a canal: Panama"
+left = 0
+right = len(s) - 1
+while left < right:
+    while left < right and not (
+        ('a' <= s[left] <= 'z') or
+        ('A' <= s[left] <= 'Z') or
+        ('0' <= s[left] <= '9')
+    ):
+        left += 1
+
+    while left < right and not (
+        ('a' <= s[right] <= 'z') or
+        ('A' <= s[right] <= 'Z') or
+        ('0' <= s[right] <= '9')
+    ):
+        right -= 1
+
+    left_char = s[left]
+    right_char = s[right]
+
+    if 'A' <= left_char <= 'Z':
+        left_char = chr(ord(left_char) + 32)
+
+    if 'A' <= right_char <= 'Z':
+        right_char = chr(ord(right_char) + 32)
+
+    if left_char != right_char:
+        print(False)
+        break
+
+    left += 1
+    right -= 1
+
+else:
+    print(True)
+
+
