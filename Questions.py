2d558be39cddc8d96623adfde284a8bd91c36d88
sagardhok <sdhok041@gmail.com>
2026-01-25 08:50:26 +0530

    Q22

--- Questions.py
+++ Questions.py
@@ -678,3 +678,15 @@ for i in range(len(nums) - 1, -1, -1):
     suffix *= nums[i]
 
 print(ans)
+
+
+nums = [1,2,3,4]
+ans = []
+for i in range(len(nums)):
+        prod = 1
+        for j in range(len(nums)):
+            if i != j:
+                prod *= nums[j]
+        ans.append(prod)
+print(ans)
+
