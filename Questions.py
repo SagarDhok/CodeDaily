2ef54ead5cd6931c4388c439d5e82eba6c756057
sagardhok <sdhok041@gmail.com>
2026-02-09 09:07:17 +0530

    update

--- Questions.py
+++ Questions.py
@@ -537,120 +537,120 @@
 #! 📐 TWO POINTERS
 # 18. Remove Duplicates from Sorted Array
 
-nums = [0,0,1,1,1,2,2,3,3,4]
+# nums = [0,0,1,1,1,2,2,3,3,4]
 
-if len(nums) == 0:
-    print(0)
+# if len(nums) == 0:
+#     print(0)
 
-i = 0  
+# i = 0  
 
-for j in range(1, len(nums)):  
-    if nums[j] != nums[i]:
-        i += 1
-        nums[i] = nums[j]
+# for j in range(1, len(nums)):  
+#     if nums[j] != nums[i]:
+#         i += 1
+#         nums[i] = nums[j]
 
-print(nums[:i+1])
-print(nums[:i+1])
+# print(nums[:i+1])
+# print(nums[:i+1])
 
 
 
 #! 19. Squares of a Sorted Array
 # Return a sorted array of squares.
 
-lst = [-4,-1,0,3,10]
-for i in range(len(lst)):
-     lst[i] = lst[i]*lst[i]
+# lst = [-4,-1,0,3,10]
+# for i in range(len(lst)):
+#      lst[i] = lst[i]*lst[i]
 
-lst.sort()
-print(lst)
-
-
-nums = [-4, -1, 0, 3, 10]
-
-n = len(nums)
-ans = [0] * n
-
-i = 0
-j = n - 1
-k = n - 1
-
-while i <= j:
-    if abs(nums[i]) > abs(nums[j]):
-        ans[k] = nums[i] * nums[i]
-        i += 1
-    else:
-        ans[k] = nums[j] * nums[j]
-        j -= 1
-    k -= 1
-
-print(ans)
+# lst.sort()
+# print(lst)
 
 
+# nums = [-4, -1, 0, 3, 10]
 
-def sortedSquares(nums):
-    left, right = 0, len(nums) - 1
-    res = []
+# n = len(nums)
+# ans = [0] * n
 
-    while left <= right:
-        if nums[left] * nums[left] > nums[right] * nums[right]:
-            res.append(nums[left] * nums[left])
-            left += 1
-        else:
-            res.append(nums[right] * nums[right])
-            right -= 1
+# i = 0
+# j = n - 1
+# k = n - 1
 
-    return res[::-1]
-print(sortedSquares( [-4, -1, 0, 3, 10]))
+# while i <= j:
+#     if abs(nums[i]) > abs(nums[j]):
+#         ans[k] = nums[i] * nums[i]
+#         i += 1
+#     else:
+#         ans[k] = nums[j] * nums[j]
+#         j -= 1
+#     k -= 1
+
+# print(ans)
+
+
+
+# def sortedSquares(nums):
+#     left, right = 0, len(nums) - 1
+#     res = []
+
+#     while left <= right:
+#         if nums[left] * nums[left] > nums[right] * nums[right]:
+#             res.append(nums[left] * nums[left])
+#             left += 1
+#         else:
+#             res.append(nums[right] * nums[right])
+#             right -= 1
+
+#     return res[::-1]
+# print(sortedSquares( [-4, -1, 0, 3, 10]))
 
 
 
 #! 20. Subarray Sum Equals K
 # Count subarrays whose sum equals k.
 
-def subarraySum(nums, k):
-    count = 0
-    n = len(nums)
+# def subarraySum(nums, k):
+#     count = 0
+#     n = len(nums)
 
-    for i in range(n):
-        s = 0
-        for j in range(i, n):
-            s += nums[j]
-            if s == k:
-                count += 1
+#     for i in range(n):
+#         s = 0
+#         for j in range(i, n):
+#             s += nums[j]
+#             if s == k:
+#                 count += 1
 
-    return count
+#     return count
 
-nums = [3, 4, 7, 2, -3, 1, 4, 2]
-k = 7
-print(subarraySum(nums, k))  
+# nums = [3, 4, 7, 2, -3, 1, 4, 2]
+# k = 7
+# print(subarraySum(nums, k))  
 
-nums = [1, 1, 1]
-k = 2
-print(subarraySum(nums, k)) 
+# nums = [1, 1, 1]
+# k = 2
+# print(subarraySum(nums, k)) 
 
 #! 21. Group Anagrams
 
 # Group strings that are anagrams.
 
-from collections import defaultdict
+# from collections import defaultdict
 
-class Solution:
-    def groupAnagrams(self, strs):
-        groups = defaultdict(list)
+# class Solution:
+#     def groupAnagrams(self, strs):
+#         groups = defaultdict(list)
 
-        for s in strs:
-            key = ''.join(sorted(s))
-            groups[key].append(s)
+#         for s in strs:
+#             key = ''.join(sorted(s))
+#             groups[key].append(s)
 
-        return list(groups.values())
+#         return list(groups.values())
 
-s = Solution()
-print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+# s = Solution()
+# print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 
 
-from collections import defaultdict
-groups = defaultdict(list)
-print(groups)
+# from collections import defaultdict
+# groups = defaultdict(list)
+# print(groups)
 
 
 
@@ -663,32 +663,32 @@ print(groups)
 # nums = [1,2,3,4]
 #Output:[24,12,8,6]
 
-nums = [1,2,3,4]
-ans = [1] * len(nums)
+# nums = [1,2,3,4]
+# ans = [1] * len(nums)
 
-prefix = 1
-for i in range(len(nums)):
-    ans[i] = prefix
-    prefix *= nums[i]
+# prefix = 1
+# for i in range(len(nums)):
+#     ans[i] = prefix
+#     prefix *= nums[i]
 
-# [1, 1, 2, 6]
-suffix = 1
-for i in range(len(nums) - 1, -1, -1):
-    ans[i] *= suffix
-    suffix *= nums[i]
+# # [1, 1, 2, 6]
+# suffix = 1
+# for i in range(len(nums) - 1, -1, -1):
+#     ans[i] *= suffix
+#     suffix *= nums[i]
 
-print(ans)
+# print(ans)
 
 
-nums = [1,2,3,4]
-ans = []
-for i in range(len(nums)):
-        prod = 1
-        for j in range(len(nums)):
-            if i != j:
-                prod *= nums[j]
-        ans.append(prod)
-print(ans)
+# nums = [1,2,3,4]
+# ans = []
+# for i in range(len(nums)):
+#         prod = 1
+#         for j in range(len(nums)):
+#             if i != j:
+#                 prod *= nums[j]
+#         ans.append(prod)
+# print(ans)
 
 
 
@@ -717,22 +717,22 @@ print(ans)
 # All numbers in nums are unique
 # nums contains numbers from [0, n] with one missing
 
-nums = [9,6,4,2,3,5,7,0,1]
+# nums = [9,6,4,2,3,5,7,0,1]
 
-for i in range(len(nums)+1):
-    if i not in nums:
-        print(i)
+# for i in range(len(nums)+1):
+#     if i not in nums:
+#         print(i)
 
 
-nums = [9,6,4,2,3,5,7,0,1]
+# nums = [9,6,4,2,3,5,7,0,1]
 
-n = len(nums)
-expected_sum = n * (n + 1) // 2  #formula to calculate sum of natural numbers 
+# n = len(nums)
+# expected_sum = n * (n + 1) // 2  #formula to calculate sum of natural numbers 
 
-print(expected_sum)
-actual_sum = sum(nums)
+# print(expected_sum)
+# actual_sum = sum(nums)
 
-print(expected_sum - actual_sum)
+# print(expected_sum - actual_sum)
 
 
 
@@ -799,14 +799,14 @@ print(expected_sum - actual_sum)
 # Output: 9
 
 
-nums = [3, 7, 2, 9, 4]
+# nums = [3, 7, 2, 9, 4]
 
-max_val = nums[0]
-for num in nums:
-    if num > max_val:
-        max_val = num
+# max_val = nums[0]
+# for num in nums:
+#     if num > max_val:
+#         max_val = num
 
-print(max_val)
+# print(max_val)
 
 
 
@@ -839,24 +839,24 @@ print(max_val)
 
 
 
-lst = [3, 7, 2, 9, 4]
-lst.sort()
-print("Second Max :",list(set(lst))[-2])
+# lst = [3, 7, 2, 9, 4]
+# lst.sort()
+# print("Second Max :",list(set(lst))[-2])
 
 
-lst = [3, 7, 2, 9, 4]
+# lst = [3, 7, 2, 9, 4]
 
-mv = float('-inf')
-sv = float('-inf')
+# mv = float('-inf')
+# sv = float('-inf')
 
-for i in lst:
-    if i > mv:
-        sv = mv
-        mv = i
-    elif i != mv and i > sv:
-        sv = i
+# for i in lst:
+#     if i > mv:
+#         sv = mv
+#         mv = i
+#     elif i != mv and i > sv:
+#         sv = i
 
-print(sv if sv != float('-inf') else -1)
+# print(sv if sv != float('-inf') else -1)
 
 
 
@@ -885,19 +885,19 @@ print(sv if sv != float('-inf') else -1)
 # def first_non_repeating(nums):
 #     pass
 
-def first_non_repeating(nums):
-   count ={}
-   for i in nums:
-      count[i] = count.get(i,0)+1
-   for key in nums:
-       if count[key]==1:
-           return key
-   return -1
+# def first_non_repeating(nums):
+#    count ={}
+#    for i in nums:
+#       count[i] = count.get(i,0)+1
+#    for key in nums:
+#        if count[key]==1:
+#            return key
+#    return -1
            
-res = first_non_repeating([4, 5, 1, 2, 0, 4])
-print(f"First Non Repeating Numbers are : {res}")
-res = first_non_repeating([1, 1, 2, 2])
-print(f"First Non Repeating Numbers are : {res}")
+# res = first_non_repeating([4, 5, 1, 2, 0, 4])
+# print(f"First Non Repeating Numbers are : {res}")
+# res = first_non_repeating([1, 1, 2, 2])
+# print(f"First Non Repeating Numbers are : {res}")
 
 
 #!Q27) You’re given a string s.
@@ -928,26 +928,26 @@ print(f"First Non Repeating Numbers are : {res}")
 # Write clean, readable code
 
 
-def first_non_repeating_char(s):
+# def first_non_repeating_char(s):
 
 
-    freq = {}
-    for c in s:
-        freq[c]=freq.get(c,0)+1
+#     freq = {}
+#     for c in s:
+#         freq[c]=freq.get(c,0)+1
 
-    for c in s:
-        if freq[c]==1:
-            return i
+#     for c in s:
+#         if freq[c]==1:
+#             return i
 
-    return None
+#     return None
 
-s = "swiss"       
-res = first_non_repeating_char(s)
-print("first non-repeating character in the string : ",res)
+# s = "swiss"       
+# res = first_non_repeating_char(s)
+# print("first non-repeating character in the string : ",res)
 
-print(first_non_repeating_char("Python"))
+# print(first_non_repeating_char("Python"))
 
-print(first_non_repeating_char("aabbcc"))
+# print(first_non_repeating_char("aabbcc"))
 
 
 
@@ -977,63 +977,63 @@ print(first_non_repeating_char("aabbcc"))
 # Time complexity should be O(n)
 # Use only Python standard features
 
-def find_duplicates(nums):
-   seen = set()
-   added = set()
-   res = []
-   for val in nums:
-       if val in seen and val not in added:
-           res.append(val)
-           added.add(val)
-       else:
-           seen.add(val)
-   return res
+# def find_duplicates(nums):
+#    seen = set()
+#    added = set()
+#    res = []
+#    for val in nums:
+#        if val in seen and val not in added:
+#            res.append(val)
+#            added.add(val)
+#        else:
+#            seen.add(val)
+#    return res
  
-print(find_duplicates([1, 2, 3, 2, 4, 1, 5]))
+# print(find_duplicates([1, 2, 3, 2, 4, 1, 5]))
 
 
 
-#! Q29) You are given a list of integers nums.
-# Task
-# Write a function that returns the count of each number, but only for numbers that appear more than once,
-# and preserve the order of their first appearance.
-# Return the result as a list of tuples: (number, count).
-# If no number repeats, return an empty list.
-# Examples
-# nums = [1, 2, 3, 2, 4, 1, 5]
-# output = [(1, 2), (2, 2)]
-# nums = [5, 5, 5, 3, 3]
-# output = [(5, 3), (3, 2)]
-# nums = [1, 2, 3]
-# output = []
-# Function signature
+# #! Q29) You are given a list of integers nums.
+# # Task
+# # Write a function that returns the count of each number, but only for numbers that appear more than once,
+# # and preserve the order of their first appearance.
+# # Return the result as a list of tuples: (number, count).
+# # If no number repeats, return an empty list.
+# # Examples
+# # nums = [1, 2, 3, 2, 4, 1, 5]
+# # output = [(1, 2), (2, 2)]
+# # nums = [5, 5, 5, 3, 3]
+# # output = [(5, 3), (3, 2)]
+# # nums = [1, 2, 3]
+# # output = []
+# # Function signature
+# # def count_duplicates(nums):
+# #     pass
+# # Rules
+# # Do NOT sort the list
+# # Do NOT use count() inside a loop
+# # Preserve order of first appearance
+# # Be ready to explain time & space complexity
+
+
 # def count_duplicates(nums):
-#     pass
-# Rules
-# Do NOT sort the list
-# Do NOT use count() inside a loop
-# Preserve order of first appearance
-# Be ready to explain time & space complexity
+#     count = {}
+#     for val in nums:
+#         count[val] = count.get(val, 0) + 1
+
+#     res = []
+#     added = set()
+
+#     for val in nums:
+#         if count[val] > 1 and val not in added:
+#             res.append((val, count[val]))
+#             added.add(val)
+
+#     return res
 
 
-def count_duplicates(nums):
-    count = {}
-    for val in nums:
-        count[val] = count.get(val, 0) + 1
-
-    res = []
-    added = set()
-
-    for val in nums:
-        if count[val] > 1 and val not in added:
-            res.append((val, count[val]))
-            added.add(val)
-
-    return res
-
-
-print(count_duplicates([1, 2, 3, 2, 4, 1, 5]))
-print(count_duplicates( [5, 5, 5, 3, 3]))
+# print(count_duplicates([1, 2, 3, 2, 4, 1, 5]))
+# print(count_duplicates( [5, 5, 5, 3, 3]))
 
 
 
@@ -1074,20 +1074,62 @@ print(count_duplicates( [5, 5, 5, 3, 3]))
 # def stable_even_odd(nums):
 #     pass
 
-def stable_even_odd(nums):
-    evens = []
-    odds = []
+# def stable_even_odd(nums):
+#     evens = []
+#     odds = []
 
-    for val in nums:
-        if val % 2 == 0:
-            evens.append(val)
-        else:
-            odds.append(val)
+#     for val in nums:
+#         if val % 2 == 0:
+#             evens.append(val)
+#         else:
+#             odds.append(val)
 
-    return evens + odds
+#     return evens + odds
 
 
-print(stable_even_odd([1, 2, 3, 4, 5, 6]))
-print(stable_even_odd([1, 3,5]))
-print(stable_even_odd([2,4,6]))
-print(stable_even_odd([]))
+# print(stable_even_odd([1, 2, 3, 4, 5, 6]))
+# print(stable_even_odd([1, 3,5]))
+# print(stable_even_odd([2,4,6]))
+# print(stable_even_odd([]))
+
+
+#! You are given a string s
+# Task
+# Write a function that checks whether the string is a palindrome,
+# ignoring case and spaces.
+# Do NOT use slicing (s[::-1])
+# Do NOT use extra libraries
+# Return True or False
+# Examples
+# "madam"                → True
+# "Never odd or even"    → True
+# "Hello"                → False
+# ""                     → True
+# Function signature
+# def is_palindrome(s):
+#     pass
+
+# Rules
+
+# Use basic Python only
+
+# Be ready to explain time & space complexity
+
+# Clean, readable logic
+
+
+def is_palindrome(s):
+    s = s.replace(" ", "").lower()
+    left = 0
+    right = len(s)-1
+    while left < right:
+        if s[left]!=s[right]:
+           return False
+        left +=1
+        right-=1
+    return True
+
+print(is_palindrome("Never odd or even"))
+print(is_palindrome("madam"))
+print(is_palindrome("Hello"))
+print(is_palindrome(""))
