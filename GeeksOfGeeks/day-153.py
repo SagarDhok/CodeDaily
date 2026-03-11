923e0925d1cc66ec8b7bc1b17f8d887bcc67e40d
sagardhok <sdhok041@gmail.com>
2026-03-11 09:00:42 +0530

    Day-153

---
+++ GeeksOfGeeks/day-153.py
@@ -0,0 +1,26 @@
+
+
+
+class Solution:
+    def floorSqrt(self, n): 
+        left = 1
+        right = n
+        ans = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+
+            if square == n:
+                return mid
+
+            if square < n:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
+s = Solution()
+n = int(input("Enter A Number : "))
+print(s.floorSqrt(n))
