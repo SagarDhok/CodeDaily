3abca0ed76495f76512a9966dd1b4d4c054ae303
sagardhok <sdhok041@gmail.com>
2026-03-22 21:07:16 +0530

    Day-147

---
+++ Leetcode/day-147.py
@@ -0,0 +1,82 @@
+
+#! 130. Surrounded Regions
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+
+# Connect: A cell is connected to adjacent cells horizontally or vertically.
+# Region: To form a region connect every 'O' cell.
+# Surround: A region is surrounded if none of the 'O' cells in that region are on the edge of the board. Such regions are completely enclosed by 'X' cells.
+# To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
+
+ 
+
+# Example 1:
+
+# Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+
+# Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+
+# Explanation:
+
+
+# In the above diagram, the bottom region is not captured because it is on the edge of the board and cannot be surrounded.
+
+# Example 2:
+
+# Input: board = [["X"]]
+
+# Output: [["X"]]
+
+ 
+
+# Constraints:
+
+# m == board.length
+# n == board[i].length
+# 1 <= m, n <= 200
+# board[i][j] is 'X' or 'O'.
+
+class Solution:
+    def solve(self, board):
+
+        if not board:
+            return
+
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != 'O':
+                return
+            
+            board[r][c] = 'S'
+
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+
+        for i in range(rows):
+            dfs(i, 0)
+            dfs(i, cols-1)
+
+        for j in range(cols):
+            dfs(0, j)
+            dfs(rows-1, j)
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'S':
+                    board[i][j] = 'O'
+
+s = Solution()
+board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+print(s.solve(board))
+
+board = [["X"]]
+print(s.solve(board))
+
