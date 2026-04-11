4554accab370a2cb42ddd61b16f3e9bf6ce94ff9
sagardhok <sdhok041@gmail.com>
2026-04-11 20:16:17 +0530

    Day-170

---
+++ GeeksOfGeeks/day-170.py
@@ -0,0 +1,24 @@
+
+
+#  Median of Two Sorted Arrays
+
+
+def median(nums1, nums2):
+    nums3 = nums1 + nums2
+    for i in range(len(nums3)):
+        for j  in range(i+1,len(nums3)):
+            if nums3[i]>nums3[j] :
+                nums3[i],nums3[j] = nums3[j],nums3[i]
+
+
+    n = len(nums3)
+    mid = n // 2
+
+    if n % 2 != 0: 
+        return nums3[mid]
+    else:  
+        return (nums3[mid - 1] + nums3[mid]) / 2
+
+nums1 = [0,1,7,9,2,4,3]
+nums2 = [8,6,10]
+print(median(nums1, nums2))
