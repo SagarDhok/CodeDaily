0ebe77b05e256d32a490978a4a884e1a59f138f9
sagardhok <sdhok041@gmail.com>
2026-05-12 10:29:54 +0530

    Day-189

---
+++ GeeksOfGeeks/day-189.py
@@ -0,0 +1,38 @@
+
+
+#! Armstrong Numbers
+# Difficulty: EasyAccuracy: 49.88%Submissions: 233K+Points: 2
+# You are given a 3-digit number n, Find whether it is an Armstrong number or not.
+
+# An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371. 
+
+# Examples:
+
+# Input: n = 153
+# Output: true
+# Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. 
+# Input: n = 372
+# Output: false
+# Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378. 
+# Input: n = 100
+# Output: false
+# Explanation: 100 is not an Armstrong number since 13 + 03 + 03 = 1. 
+# Constraints:
+# 100 ≤ n <1000 
+
+class Solution:
+    def armstrongNumber (self, n):
+        cube = 0
+        temp_num = n
+        while temp_num>0:
+            d = temp_num%10
+            cube+=d**3
+            temp_num//=10
+        if cube==n:
+            return True
+        else:
+            return False
+            
+s = Solution()
+n = int(input("Enter A Number : "))
+print(s.armstrongNumber(n))
