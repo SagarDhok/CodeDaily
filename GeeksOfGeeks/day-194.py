4ac227d2cb9de38dbd003b8e637059fb40fbd091
sagardhok <sdhok041@gmail.com>
2026-05-18 21:52:54 +0530

    Day-194

---
+++ GeeksOfGeeks/day-194.py
@@ -0,0 +1,49 @@
+
+
+
+#! Sum 1 to n Divisors
+# Difficulty: EasyAccuracy: 43.37%Submissions: 225K+Points: 2
+# Given a positive integer n, The task is to find the value of Σi F(i) where i is from 1 to n and function F(i) is defined as the sum of all divisors of i.
+
+# Examples:
+
+# Input: n = 4
+# Output: 15
+# Explanation:
+# F(1) = 1
+# F(2) = 1 + 2 = 3
+# F(3) = 1 + 3 = 4
+# F(4) = 1 + 2 + 4 = 7
+# So, F(1) + F(2) + F(3) + F(4)
+#     = 1 + 3 + 4 + 7 = 15
+# Input: n = 5
+# Output: 21
+# Explanation:
+# F(1) = 1
+# F(2) = 1 + 2 = 3
+# F(3) = 1 + 3 = 4
+# F(4) = 1 + 2 + 4 = 7
+# F(5) = 1 + 5 = 6
+# So,  F(1) + F(2) + F(3) + F(4) + F(5)
+#     = 1 + 3 + 4 + 7 + 6 = 21
+# Input: n = 1
+# Output: 1
+# Explanation:
+# F(1) = 1
+# So,  F(1) = 1 
+# Constraints:
+# 1 <= n <= 105
+
+
+class Solution:
+    def sumOfDivisors(self, n):
+        total = 0
+        
+       
+        for i in range(1, n + 1):
+            total += i * (n // i)
+            
+        return total
+s = Solution()
+n  = int(input("Entet A Number : "))
+print(s.sumOfDivisors(n))
