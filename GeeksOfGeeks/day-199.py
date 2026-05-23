582d0c1a01e9cc8f25db078724d0ef700dd7ba72
sagardhok <sdhok041@gmail.com>
2026-05-23 08:44:08 +0530

    Day-199

---
+++ GeeksOfGeeks/day-199.py
@@ -0,0 +1,106 @@
+
+
+Search...
+Courses
+Tutorials
+Practice
+Jobs
+
+avatar
+
+
+#! Alien Dictionary
+# Difficulty: HardAccuracy: 47.81%Submissions: 243K+Points: 8
+# A new alien language uses the English alphabet, but the order of letters is unknown. You are given a list of words[] from the alien language’s dictionary, where the words are claimed to be sorted lexicographically according to the language’s rules.
+
+# Your task is to determine the correct order of letters in this alien language based on the given words. If the order is valid, return a string containing the unique letters in lexicographically increasing order as per the new language's rules. If there are multiple valid orders, return any one of them.
+
+# However, if the given arrangement of words is inconsistent with any possible letter ordering, return an empty string ("").
+
+# A string a is lexicographically smaller than a string b if, at the first position where they differ, the character in a appears earlier in the alien language than the corresponding character in b. If all characters in the shorter word match the beginning of the longer word, the shorter word is considered smaller.
+
+# Note: Your implementation will be tested using a driver code. It will print true if your returned order correctly follows the alien language’s lexicographic rules; otherwise, it will print false.
+
+# # Examples:
+
+# # Input: words[] = ["baa", "abcd", "abca", "cab", "cad"]
+# # Output: true
+# # Explanation: A possible correct order of letters in the alien dictionary is "bdac".
+# # The pair "baa" and "abcd" suggests 'b' appears before 'a' in the alien dictionary.
+# # The pair "abcd" and "abca" suggests 'd' appears before 'a' in the alien dictionary.
+# # The pair "abca" and "cab" suggests 'a' appears before 'c' in the alien dictionary.
+# # The pair "cab" and "cad" suggests 'b' appears before 'd' in the alien dictionary.
+# # So, 'b' → 'd' → 'a' → 'c' is a valid ordering.
+# # Input: words[] = ["caa", "aaa", "aab"]
+# # Output: true
+# # Explanation: A possible correct order of letters in the alien dictionary is "cab".
+# # The pair "caa" and "aaa" suggests 'c' appears before 'a'.
+# # The pair "aaa" and "aab" suggests 'a' appear before 'b' in the alien dictionary. 
+# # So, 'c' → 'a' → 'b' is a valid ordering.
+# # Input: words[] = ["ab", "cd", "ef", "ad"]
+# # Output: ""
+# # Explanation: No valid ordering of letters is possible.
+# # The pair "ab" and "ef" suggests "a" appears before "e".
+# # The pair "ef" and "ad" suggests "e" appears before "a", which contradicts the ordering rules.
+# # Constraints:
+# # 1 ≤ words.length ≤ 500
+# # 1 ≤ words[i].length ≤ 100
+# # words[i] consists only of lowercase English letters.
+
+
+
+from collections import defaultdict, deque
+
+class Solution:
+    def findOrder(self, words):
+
+        graph = defaultdict(set)
+        indegree = defaultdict(int)
+
+        for word in words:
+            for ch in word:
+                indegree[ch] = 0
+
+        for i in range(len(words) - 1):
+            w1 = words[i]
+            w2 = words[i + 1]
+
+            if len(w1) > len(w2) and w1.startswith(w2):
+                return ""
+
+            for j in range(min(len(w1), len(w2))):
+                if w1[j] != w2[j]:
+                    if w2[j] not in graph[w1[j]]:
+                        graph[w1[j]].add(w2[j])
+                        indegree[w2[j]] += 1
+                    break
+
+        q = deque()
+
+        for ch in indegree:
+            if indegree[ch] == 0:
+                q.append(ch)
+
+        order = []
+
+        while q:
+            node = q.popleft()
+            order.append(node)
+
+            for nei in graph[node]:
+                indegree[nei] -= 1
+
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        if len(order) != len(indegree):
+            return ""
+
+        return "".join(order)
+
+s = Solution()
+words = ["caa", "aaa", "aab"]
+print(s.findOrder(words))
+
+words = ["ab", "cd", "ef", "ad"]
+print(s.findOrder(words))
