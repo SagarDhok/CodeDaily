f4b74bc1b608a6c68209f9cc80da0444f39579cc
sagardhok <sdhok041@gmail.com>
2026-06-03 08:02:37 +0530

    Day-150

---
+++ Leetcode/day-150.py
@@ -0,0 +1,41 @@
+
+
+#! 342. Power of Four
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+# An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+
+# Example 1:
+
+# Input: n = 16
+# Output: true
+# Example 2:
+
+# Input: n = 5
+# Output: false
+# Example 3:
+
+# Input: n = 1
+# Output: true
+
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n % 4 == 0:
+            n //= 4
+
+        return n == 1
+s  = Solution()
+n = int(input("Enter A Number : "))
+obj =s.isPowerOfFour(n)
+print(obj)
