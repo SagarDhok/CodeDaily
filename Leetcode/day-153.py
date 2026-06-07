3b44ba2e407b162b43ec7d07af929bab45cfce86
sagardhok <sdhok041@gmail.com>
2026-06-07 23:27:49 +0530

    Day-153

---
+++ Leetcode/day-153.py
@@ -0,0 +1,70 @@
+
+#! 97. Interleaving String
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+
+# An interleaving of two strings s and t is a configuration where s and t are divided into n and m substrings respectively, such that:
+
+# s = s1 + s2 + ... + sn
+# t = t1 + t2 + ... + tm
+# |n - m| <= 1
+# The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+# Note: a + b is the concatenation of strings a and b.
+
+ 
+
+# Example 1:
+
+
+# Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+# Output: true
+# Explanation: One way to obtain s3 is:
+# Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
+# Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
+# Since s3 can be obtained by interleaving s1 and s2, we return true.
+# Example 2:
+
+# Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+# Output: false
+# Explanation: Notice how it is impossible to interleave s2 with any other string to obtain s3.
+# Example 3:
+
+# Input: s1 = "", s2 = "", s3 = ""
+# Output: true
+
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        m = len(s1)
+        n = len(s2)
+
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+
+        for i in range(m + 1):
+            for j in range(n + 1):
+
+                if i > 0 and s1[i - 1] == s3[i + j - 1]:
+                    dp[i][j] = dp[i][j] or dp[i - 1][j]
+
+                if j > 0 and s2[j - 1] == s3[i + j - 1]:
+                    dp[i][j] = dp[i][j] or dp[i][j - 1]
+
+        return dp[m][n]
+s = Solution()
+s1 = "aabcc"
+s2 = "dbbca"
+s3 = "aadbbcbcac"
+print(s.isInterleave(s1,s2,s3))
+
+s1 = "aabcc"
+s2 = "dbbca"
+s3 = "aadbbbaccc"
+print(s.isInterleave(s1,s2,s3))
