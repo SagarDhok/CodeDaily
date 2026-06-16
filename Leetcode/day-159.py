512330503bd6c477fbbdb9ed07a8743a1026ce07
sagardhok <sdhok041@gmail.com>
2026-06-16 08:09:00 +0530

    Day-159

---
+++ Leetcode/day-159.py
@@ -0,0 +1,55 @@
+
+#! 459. Repeated Substring Pattern
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+ 
+
+# Example 1:
+
+# Input: s = "abab"
+# Output: true
+# Explanation: It is the substring "ab" twice.
+# Example 2:
+
+# Input: s = "aba"
+# Output: false
+# Example 3:
+
+# Input: s = "abcabcabcabc"
+# Output: true
+# Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of lowercase English letters.
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+
+        len_s = len(s)
+
+        for val in range(1, len_s // 2 + 1):
+
+            if len_s % val == 0:
+
+                pattern = s[:val]
+                times = len_s // val
+
+                if pattern * times == s:
+                    return True
+
+        return False
+
+sol = Solution()
+s = input("Enter String : ")
+print(sol.repeatedSubstringPattern(s))
+
+
+                       
